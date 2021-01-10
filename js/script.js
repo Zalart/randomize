3987124b0069850afe1c4d69d68129033b70fa5a
artur <me@zalart.com>
2021-01-11 02:38:31 +0300

    Pseudoslides implemented. Presidents polulated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+function getAllPresidents() {
+    let result = [];
+    let presidentsRaws = document.querySelectorAll('.wikitable tbody tr');
+    presidentsRaws.forEach(element => {
+        // if (element.querySelectorAll('td') !== undefined)
+        let presidentsTds = element.querySelectorAll('th ~ td');
+        //alert(presidentsTd[1].innerHTML);
+        if (presidentsTds.length > 2) {
+            let presidency = "";
+            let presidentPhoto = "";
+            let presidentName = "";
+            for (let i = 0; i < 3; i++) {
+                if (i === 0) {
+                    let presidencyData = presidentsTds[i].querySelectorAll('span');
+                    // if (presidencyData === null) continue;
+
+                    presidencyData.forEach(element => {
+                        presidency === "" ? presidency = element.innerText + ' - ' : presidency += element.innerText;
+
+                    });
+                    //  result.push({ 'presidency': presidency });
+                } else if (i === 1) {
+
+                    //   if (presidentsTds[i].querySelector('img').src === null) continue;
+                    presidentPhoto = presidentsTds[i].querySelector('img').src;
+                    //  alert(presidentPhoto);
+                    // result.push({ 'photo': presidentPhoto });
+                } else if (i === 2) {
+                    presidentName = presidentsTds[i].querySelector('a').innerHTML;
+                    //  if (presidentName === null) continue;
+
+                }
+
+            }
+            // In case we need an array of objects 
+            result.push({ 'presidency': presidency, 'photo': presidentPhoto, 'name': presidentName });
+        }
+    });
+    return result;
+}
 /*function dotschange(i) {
     const dots = document.querySelectorAll(".dot");
     logo.classList.add("shake");
@@ -45,15 +85,24 @@ function randotschange(n) {
                 }
                 break;
         }
+        document.querySelector("h2").innerHTML = `Math.random() result is: ${n}`;
         logo.classList.remove("shake");
     }, 1000)
 }
-
+const presidents = document.querySelector(".presidents");
+const dices = document.querySelector(".dices");
 const button = document.querySelectorAll('.button');
 const logo = document.querySelector(".logo");
-button.forEach((element) => {
-    element.addEventListener("click", () => {
-        const n = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-        randotschange(n);
-    })
+button[0].addEventListener("click", () => {
+    presidents.classList.add("toggle");
+    dices.classList.remove("toggle");
+    const n = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    randotschange(n);
+});
+button[1].addEventListener("click", () => {
+
+
+    presidents.classList.remove("toggle");
+    dices.classList.add("toggle");
+
 });
