92dd77d00953fcc23a5f98387680cc1903a463e3
artur <me@zalart.com>
2021-01-10 03:19:25 +0300

    Random dices, shadows under the buttons

--- js/script.js
+++ js/script.js
@@ -1,57 +1,59 @@
-//  set your counter to 1
-
-function dotschange(i) {
+/*function dotschange(i) {
     const dots = document.querySelectorAll(".dot");
-    //  create a loop function
     logo.classList.add("shake");
     setTimeout(function () {
-        //  call a 3s setTimeout when the loop is called
-        //  your code here
-        //  increment the counter
         if (i < 7) {
             if (i === 0) {
-                dots[i].classList.add("black");
+                dots[i].classList.add("white");
                 i++;
                 dotschange(i);
             } else if (i !== 6) {
-                dots[i].classList.add("black");
-                let prev = i - 1;
-                dots[prev].classList.remove("black");
-                i++;            //  if the counter < 10, call the loop function
+                dots[i].classList.add("white");
+                dots[i - 1].classList.remove("white");
+                i++;
                 dotschange(i);
             } else {
-                dots[5].classList.remove("black");
+                dots[5].classList.remove("white");
                 i++;
                 logo.classList.remove("shake");
-                //  if the counter < 10, call the loop function
             }
-            //  ..  again which will trigger another 
         }
-
-        //  ..  setTimeout()
     }, 400)
+}*/
+
+function randotschange(n) {
+    const dots = document.querySelectorAll(".dot");
+    logo.classList.add("shake");
+    dots.forEach(element => {
+        element.classList.remove("white");
+    });
+    setTimeout(function () {
+        switch (n) {
+            case 2:
+                for (let i = 1; i < (n + 1); i++) {
+                    dots[i].classList.add("white");
+                }
+                break;
+            case 4:
+                for (let i = 1; i < (n + 1); i++) {
+                    dots[i].classList.add("white");
+                }
+                break;
+            default:
+                for (let i = 0; i < n; i++) {
+                    dots[i].classList.add("white");
+                }
+                break;
+        }
+        logo.classList.remove("shake");
+    }, 1000)
 }
 
-//  start the loop
-/*
-function dotschange() {
-    
-    for (let i = 0; i < dots.length; i++) {
-
-
-        setTimeout(function () {
-            
-            alert("test");
-        }, i * 1000);
-
-        dices[i].classList.remove("black");
-
-    }
-} */
 const button = document.querySelectorAll('.button');
 const logo = document.querySelector(".logo");
 button.forEach((element) => {
     element.addEventListener("click", () => {
-        dotschange(0);
+        const n = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+        randotschange(n);
     })
 });
